cc90fa800bf7ef2d2268bf72fd6c426b6f23d066
Sebastian Cerdas <70732029+Sebastian2207@users.noreply.github.com>
2023-02-17 11:05:09 -0600

    admin sebas

--- db_devTec.sql
+++ db_devTec.sql
@@ -69,7 +69,7 @@ insert into usuarios (nombre, primApellido,segApellido,correo, contrasena,rol,nu
 values ('Derek','Leiva','Villalobos','electronico@gmail.com','asd123asd123ads','cliente','88884444','activo');
 
 insert into usuarios (nombre, primApellido,segApellido,correo, contrasena,rol,numTelefono,status) 
-values ('Sebastian','Cerdas','Cedeño','sebas.cc22@gmail.com','sebas2207','Administrador','72097414','activo');
+values ('Sebastian','Cerdas','Cedeño','sebas.cc22@gmail.com','sebas2207','administrador','72097414','activo');
 
 ----- INSERT SERVICIOS ----
 insert INTO servicio (nombreServicio, descripción, precio) values ('Cambio de pasta termica','Cambio de pasta terminca para computadora, puede ser Laptop o Desktop', 1000);
