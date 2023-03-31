74993c3aee02635ebf819c905b168893b3aa617d
Sebastian Cerdas <70732029+Sebastian2207@users.noreply.github.com>
2023-03-31 08:45:51 -0600

    servicio

--- G1_techsuport/src/main/java/com/techsupport/hub/domain/Usuario.java
+++ G1_techsuport/src/main/java/com/techsupport/hub/domain/Usuario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.techsupport.hub.domain;
 
 
@@ -47,10 +43,5 @@ public class Usuario implements Serializable{
         this.rol = rol;
         this.numtelefono = numtelefono;
         this.status = status;
-    }
-    
-    
-    
-     
-    
+    }    
 }//fin class Usuario{}
